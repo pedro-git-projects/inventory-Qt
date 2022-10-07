9bfb9a048eac6d96755745737fdc4925d9160622
pedro-git-projects <pedro.generic.email@gmail.com>
2022-10-07 16:24:22 -0300

    uniform braces styling

--- Inventory/additemdialog.cpp
+++ Inventory/additemdialog.cpp
@@ -1,14 +1,10 @@
 #include "additemdialog.h"
 #include "ui_additemdialog.h"
 
-AddItemDialog::AddItemDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::AddItemDialog)
-{
+AddItemDialog::AddItemDialog(QWidget *parent) : QDialog(parent), ui(new Ui::AddItemDialog) {
     ui->setupUi(this);
 }
 
-AddItemDialog::~AddItemDialog()
-{
+AddItemDialog::~AddItemDialog() {
     delete ui;
 }
--- Inventory/updateitemdialog.cpp
+++ Inventory/updateitemdialog.cpp
@@ -1,14 +1,10 @@
 #include "updateitemdialog.h"
 #include "ui_updateitemdialog.h"
 
-UpdateItemDialog::UpdateItemDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::UpdateItemDialog)
-{
+UpdateItemDialog::UpdateItemDialog(QWidget *parent) : QDialog(parent), ui(new Ui::UpdateItemDialog) {
     ui->setupUi(this);
 }
 
-UpdateItemDialog::~UpdateItemDialog()
-{
+UpdateItemDialog::~UpdateItemDialog() {
     delete ui;
 }
